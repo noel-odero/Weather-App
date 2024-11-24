f7d74f2a6945d95bfc16dac64024b5d6baa29288
Noel Odero <anoel.odero@gmail.com>
2024-11-24 09:25:27 +0200

    initial commit

---
+++ public/script.js
@@ -0,0 +1,46 @@
+// select input form
+const searchElement = document.querySelector('[data-city-search]');
+// create seacrhBox generated from google places api
+const searchBox = new google.maps.places.SearchBox(searchElement);
+// add listener is a google maps stuff, not js
+
+// whwnever our search box has a place selected inside of it, this code will be executed
+searchBox.addListener('places_changed', () => {
+  const place = searchBox.getPlaces()[0]; // select first place
+  // if (place == null) return; // make sure it is not null
+  const latitude = place.geometry.location.lat(); // similar to nav.geo in plain JS
+  const longitude = place.geometry.location.lng();
+
+  // using this info, our data will be posted to our server, and fetch will actually fetch that data
+  fetch('/weather', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify({
+      latitude: latitude,
+      longitude: longitude,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data, 'Our weather info');
+      setWeatherData(data, place.formatted_address);
+    });
+});
+
+console.log('this is script.js in action');
+
+const locationElt = document.querySelector('[data-location]');
+const statusElt = document.querySelector('[data-status]');
+const temperatureElt = document.querySelector('[data-temperature]');
+const precipitationElt = document.querySelector('[data-precipitation]');
+const windElt = document.querySelector('[data-wind]');
+
+function setWeatherData(data, place) {
+  locationElt.textContent = place;
+  statusElt.textContent = data.weather.description;
+  temperatureElt.textContent = data.main.temp;
+  precipitationElt.textContent = data.main.humidity;
+}
---
+++ server.js
@@ -0,0 +1,38 @@
+// check if your not running on production, then configure your environment
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+// const API_KEY = process.env.OPENWEATHER_API_KEY;
+const API_KEY = 'b172652aa01bef4790b21e6dc71262d4';
+// populate the api_key var, with your api key details
+
+const express = require('express');
+const axios = require('axios');
+// axios works similarly to fetch
+const port = 3000;
+const app = express();
+
+app.use(express.json());
+// set up express to use json, since we will be making api calls
+app.use(express.static('public'));
+// set express to know where your static folders are found
+// that is our html will be served from here
+
+app.post('/weather', (req, res) => {
+  const url = ` https://api.openweathermap.org/data/2.5/weather?lat=${req.body.latitude}&lon=${req.body.longitude}&appid=${API_KEY}`;
+  axios({
+    url: url,
+    responseType: 'json',
+  })
+    .then((data) => {
+      console.log(res.json(data.data));
+    })
+    .catch((err) => console.log(`an unexpected error happened ${err}`));
+  // this will send a json version of our data, back to the script.js
+  // console.log(req.body);
+});
+
+app.listen(port, () => {
+  console.log(`server started on port ${port}`);
+});
